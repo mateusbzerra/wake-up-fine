47dcb010e911ce1f2b6ca6a6bc8c45e4aeeb9473
Mateus Bezerra <mateusbzerra@gmail.com>
2024-11-25 22:39:29 -0300

    chore: add Nunito as default font family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,10 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import { Nunito } from "next/font/google";
+import "@styles/tailwind.global.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const defaultFontFamily = Nunito({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
 });
 
 export const metadata: Metadata = {
@@ -25,9 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${defaultFontFamily.className}  antialiased`}>
         {children}
       </body>
     </html>
